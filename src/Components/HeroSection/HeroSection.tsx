b1bfe633c74cebd6be24433ad1ba2cf432db270e
Ayoub Elhioui <elhioui001@gmail.com>
2024-02-26 20:21:53 +0100

    adding right and left divs for heroSection:

--- src/Components/HeroSection/HeroSection.tsx
+++ src/Components/HeroSection/HeroSection.tsx
@@ -3,12 +3,22 @@ import HeroSectionPicture from '../../assets/images/hero-section-picture.jpg'
 import '../../index.css'
 import './Style/HeroSection.css'
 
-const HerSection = () => {
+const HeroSection = () => {
   return(
       <section className="hero-wrapper">
         <div className="innerWidth paddings flexCenter hero-container">
           <div className="hero-left">
-            leftSection
+            <div className="hero-title">
+              <h1>
+                Discover <br/>
+                Most Suitable <br/>
+                Property
+              </h1>
+            </div>
+            <div className="hero-description">
+              <span>Find a variety of properties that suit you very easily</span>
+              <span>Forget all difficulties in finding a residence for you</span>
+            </div>
           </div>
           <div className="flexCenter hero-right">
               <div className="image-container">
@@ -20,4 +30,4 @@ const HerSection = () => {
   );
 };
 
-export default HerSection;
+export default HeroSection;
