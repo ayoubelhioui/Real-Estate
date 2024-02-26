eb8396a9556c3c17e8e08c331d29ff52eebe67d1
Ayoub Elhioui <elhioui001@gmail.com>
2024-02-26 20:39:03 +0100

    styling the orange circle

--- src/Components/HeroSection/HeroSection.tsx
+++ src/Components/HeroSection/HeroSection.tsx
@@ -5,26 +5,35 @@ import './Style/HeroSection.css'
 
 const HeroSection = () => {
   return(
-      <section className="hero-wrapper">
-        <div className="innerWidth paddings flexCenter hero-container">
-          <div className="hero-left">
-            <div className="hero-title">
+      <section className='hero-wrapper'>
+        <div className='innerWidth paddings flexCenter hero-container'>
+          <div className='flexColStart hero-left'>
+
+            <div className='hero-title'>
+              <div className='orange-circle'/>
               <h1>
                 Discover <br/>
                 Most Suitable <br/>
                 Property
               </h1>
             </div>
-            <div className="hero-description">
+
+            <div className='flexColStart hero-description'>
               <span>Find a variety of properties that suit you very easily</span>
               <span>Forget all difficulties in finding a residence for you</span>
             </div>
+
+            <div className="search-bar">
+              searchBar
+            </div>
           </div>
-          <div className="flexCenter hero-right">
-              <div className="image-container">
-                  <img src={HeroSectionPicture} alt=""/>
+
+          <div className='flexCenter hero-right'>
+              <div className='image-container'>
+                  <img src={HeroSectionPicture} alt=''/>
               </div>
           </div>
+
         </div>
       </section>
   );
